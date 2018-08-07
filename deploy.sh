f6d4fdac5217970af3c119be6fc33dde02ebb3d0
Mike Bertram <contact@mibexx.de>
2018-08-07 08:35:31 +0200

    + Deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git checkout .
+git fetch -vp
+git pull
+
+composer update
+vendor/bin/xervice da:ge
+vendor/bin/xervice pr:co:ge
+vendor/bin/xervice pr:mo:bu
+vendor/bin/xervice pr:mi
+
+npm install
+npm run build
